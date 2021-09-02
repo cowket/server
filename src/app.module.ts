43466b0589cd1edf4043feab4c2e40d22f2cc7f0
n2ptune <n2ptune@outlook.com>
2021-09-02 20:52:18 +0900

    feat: socket gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { SocketGateway } from './socket/socket.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService, SocketGateway]
 })
 export class AppModule {}
---
+++ src/socket/socket.gateway.ts
@@ -0,0 +1,44 @@
+import { Logger } from '@nestjs/common'
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer
+} from '@nestjs/websockets'
+import { Server, Socket } from 'socket.io'
+
+@WebSocketGateway(4001, {
+  transports: ['websockets'],
+  cors: {
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT'],
+    credentials: true,
+    origin: '*'
+  }
+})
+export class SocketGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  private logger = new Logger('SocketGateway')
+  @WebSocketServer()
+  server: Server
+
+  @SubscribeMessage('message')
+  handleEvent(@MessageBody() data: string, @ConnectedSocket() client: Socket): string {
+    return data
+  }
+
+  afterInit(server: Server) {
+    this.logger.log('Init Gateway')
+    this.server = server
+  }
+
+  handleConnection(socket: Socket) {
+    this.logger.log(socket.id)
+  }
+
+  handleDisconnect(socket: Socket) {
+    this.logger.log('disconnect', socket.id)
+  }
+}
