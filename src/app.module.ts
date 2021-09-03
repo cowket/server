760fb72c0b0e5a582a37e210146947fd769b3ad2
n2ptune <vue2598@gmail.com>
2021-09-03 06:16:51 +0000

    docker set

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
-import { SocketGateway } from './socket/socket.gateway';
+import { SocketGateway } from './socket/socket.gateway'
 
 @Module({
   imports: [],
--- src/socket/socket.gateway.spec.ts
+++ src/socket/socket.gateway.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { SocketGateway } from './socket.gateway';
+import { Test, TestingModule } from '@nestjs/testing'
+import { SocketGateway } from './socket.gateway'
 
 describe('SocketGateway', () => {
-  let gateway: SocketGateway;
+  let gateway: SocketGateway
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SocketGateway],
-    }).compile();
+      providers: [SocketGateway]
+    }).compile()
 
-    gateway = module.get<SocketGateway>(SocketGateway);
-  });
+    gateway = module.get<SocketGateway>(SocketGateway)
+  })
 
   it('should be defined', () => {
-    expect(gateway).toBeDefined();
-  });
-});
+    expect(gateway).toBeDefined()
+  })
+})
