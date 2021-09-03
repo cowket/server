30b5ff55647acd429bea09b32f8a73fd600f9b11
n2ptune <n2ptune@outlook.com>
2021-09-03 20:59:51 +0900

    db setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { SocketGateway } from './socket/socket.gateway'
 
+const isProd = process.env.NODE_ENV === 'production'
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: isProd ? '.env.production' : '.env',
+      isGlobal: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: process.env.DB_NAME,
+      synchronize: !isProd,
+      entities: []
+    })
+  ],
   controllers: [AppController],
   providers: [AppService, SocketGateway]
 })
