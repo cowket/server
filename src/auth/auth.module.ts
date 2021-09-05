9d0e8f2670a8b811a2ef4aca5fcc4c4a60fce2df
n2ptune <n2ptune@outlook.com>
2021-09-05 12:46:51 +0900

    local strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
+import { PassportModule } from '@nestjs/passport'
 import { UsersModule } from 'src/users/users.module'
 import { AuthService } from './auth.service'
+import { LocalStrategy } from './local.strategy'
 
 @Module({
-  providers: [AuthService],
-  imports: [UsersModule]
+  providers: [AuthService, LocalStrategy],
+  imports: [UsersModule, PassportModule]
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { UsersService } from 'src/users/users.service'
-import * as bcrypt from 'bcrypt'
+// import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class AuthService {
@@ -8,11 +8,13 @@ export class AuthService {
 
   async validateUser(email: string, pw: string): Promise<any> {
     try {
-      const user = this.usersService.findOne(email)
+      const user = await this.usersService.findOne(email)
 
-      if (!user) throw new Error('404')
+      if (!user) return null
 
       // TODO 전달된 비밀번호 <-> DB에 암호화된 비밀번호 비교 필요
+      // 테스트
+      return user
     } catch (error) {} // 에러 핸들 필요
   }
 }
