dee25e8917418be41a48d0842c3ef6f7027c71d8
n2ptune <n2ptune@outlook.com>
2021-09-05 19:45:14 +0900

    swagger module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Logger, Post, Request, UseGuards } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
+import { ApiOperation, ApiTags } from '@nestjs/swagger'
 
+@ApiTags('Auth Controller')
 @Controller('auth')
 export class AuthController {
   private logger: Logger = new Logger('AuthController')
 
   @UseGuards(AuthGuard('local'))
   @Post('login')
+  @ApiOperation({ summary: 'login (test)', description: '로그인 API - 테스트중' })
   async login(@Request() req) {
     return req.user
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Cowket Server Document')
+    .setDescription('Cowket Server Document on Swagger')
+    .setVersion('1.0')
+    .addTag('cowket')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('swagger', app, document)
+
   await app.listen(4000)
 }
 bootstrap()
