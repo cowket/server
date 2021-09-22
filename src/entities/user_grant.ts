3c14e8e219d8d1165d6f6c7b537e792969f47608
n2ptune <n2ptune@outlook.com>
2021-09-23 00:44:39 +0900

    user grant entity

--- src/entities/user_grant.ts
+++ src/entities/user_grant.ts
@@ -1,19 +1,35 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToMany,
+  PrimaryGeneratedColumn
+} from 'typeorm'
+import { Channel } from './channel'
+import { Team } from './team'
+import { User } from './user'
 
 @Entity({ name: 'user_grant' })
 export class UserGrant {
   @PrimaryGeneratedColumn()
   id: number
 
-  @Column()
-  user_id: number
-
+  @ManyToMany(() => User)
+  @JoinColumn({ referencedColumnName: 'uuid', name: 'user_uuid' })
   @Column('varchar')
-  user_uuid: string
+  user_uuid: User
 
-  @Column()
-  team_id: number
+  @ManyToMany(() => Team)
+  @JoinColumn({ referencedColumnName: 'uuid', name: 'team_uuid' })
+  @Column('varchar', { nullable: true })
+  team_uuid: Team
 
-  @Column('varchar')
-  team_uuid: string
+  @ManyToMany(() => Channel)
+  @JoinColumn({ referencedColumnName: 'uuid', name: 'channel_uuid' })
+  @Column('varchar', { nullable: true })
+  channel_uuid: Channel
+
+  @CreateDateColumn({ nullable: false })
+  create_date: Date
 }
