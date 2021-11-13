550727a7890907c1964790cd8fa34ed4d15d1377
n2ptune <n2ptune@outlook.com>
2021-11-13 18:44:44 +0900

    new reaction item

--- src/react/react.module.ts
+++ src/react/react.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Reaction } from 'src/entities/reaction'
+import { ReactionItem } from 'src/entities/reaction_item'
 import { ReactService } from './react.service'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Reaction, ReactionItem])],
   providers: [ReactService]
 })
 export class ReactModule {}
--- src/react/react.service.ts
+++ src/react/react.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Reaction } from 'src/entities/reaction'
+import { ReactionItem } from 'src/entities/reaction_item'
+import { Repository } from 'typeorm'
 
 @Injectable()
-export class ReactService {}
+export class ReactService {
+  constructor(
+    @InjectRepository(Reaction) private reactionRepo: Repository<Reaction>,
+    @InjectRepository(ReactionItem)
+    private reactionItemRepo: Repository<ReactionItem>
+  ) {}
+
+  async createNewReactionItem(reaction: string) {
+    const reactionItem = await this.reactionItemRepo.findOne({
+      where: { content: reaction }
+    })
+
+    if (reactionItem) return reactionItem
+
+    return this.reactionItemRepo.save({
+      content: reaction
+    })
+  }
+}
