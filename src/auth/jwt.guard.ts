89e513de0c1ecd07dd2b4314b560b69952f5f041
n2ptune <n2ptune@outlook.com>
2021-12-10 21:52:45 +0900

    refactor: 가드 기능 -> jwt 전략 로직으로 이관

--- src/auth/jwt.guard.ts
+++ src/auth/jwt.guard.ts
@@ -2,33 +2,4 @@ import { Injectable } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
 
 @Injectable()
-export class JwtGuard extends AuthGuard('jwt') {
-  // async handleRequest(err: any, user: DecodeTokenUser, info: Error) {
-  //   if (info && info.name === 'TokenExpiredError') {
-  //     const req = this.ctx.switchToHttp().getRequest()
-  //     const res = this.ctx.switchToHttp().getResponse()
-  //     const { authorization } = req.headers
-  //     if (!authorization) {
-  //       throw new UnauthorizedException()
-  //     } else {
-  //       const token = (authorization as string).split('Bearer ')[1]
-  //       const { uuid } = this.jwtService.decode(token) as any
-  //       const { refresh_token: refreshToken } = await this.userService.getRefreshTokenByUuid(uuid)
-  //       await this.jwtService.verifyAsync(refreshToken, {
-  //         clockTimestamp: Math.floor(Date.now() / 1000),
-  //         ignoreExpiration: false,
-  //         secret: this.configService.get('TO_SIGN')
-  //       })
-  //       const tokenUser = await this.authService.getTokenUserInfoByUuid(uuid)
-  //       const accessToken = await this.authService.genAccessToken(tokenUser)
-  //       const decodeTokenUser = this.jwtService.decode(accessToken) as DecodeTokenUser
-  //       res.setHeader('Authorization', accessToken)
-  //       return decodeTokenUser
-  //     }
-  //   }
-  //   if (!user || err) {
-  //     throw new UnauthorizedException()
-  //   }
-  //   return user
-  // }
-}
+export class JwtGuard extends AuthGuard('jwt') {}
