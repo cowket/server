f050d7f2e0ecdb01eb0ceaa36f43b6ec182f8e6f
n2ptune <n2ptune@outlook.com>
2021-12-11 22:29:29 +0900

    refactor: 팀 모듈 user_grant 레포 의존성 제거

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -5,7 +5,6 @@ import { ChannelModule } from 'src/channel/channel.module'
 import { Team } from 'src/entities/team'
 import { TeamUserProfile } from 'src/entities/team_user_profile'
 import { User } from 'src/entities/user'
-import { UserGrant } from 'src/entities/user_grant'
 import { GrantModule } from 'src/grant/grant.module'
 import { MessageModule } from 'src/message/message.module'
 import { UserModule } from 'src/user/user.module'
@@ -15,7 +14,7 @@ import { TeamService } from './team.service'
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Team, TeamUserProfile, UserGrant, User]),
+    TypeOrmModule.forFeature([Team, TeamUserProfile, User]),
     UtilModule,
     forwardRef(() => UserModule),
     ConfigModule,
