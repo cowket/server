487cfb88721172cc258a104c91f9610ac986148d
n2ptune <n2ptune@outlook.com>
2021-12-19 03:51:56 +0900

    test: alert service 테스트 클래스 임시 정의

--- src/alert/alert.service.spec.ts
+++ src/alert/alert.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { getRepositoryToken } from '@nestjs/typeorm'
+import { Alert } from 'src/entities/alert'
 import { AlertService } from './alert.service'
+
+class MockAlertRepo {}
 
 describe('AlertService', () => {
   let service: AlertService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AlertService]
+      providers: [
+        AlertService,
+        {
+          provide: getRepositoryToken(Alert),
+          useClass: MockAlertRepo
+        }
+      ]
     }).compile()
 
     service = module.get<AlertService>(AlertService)
